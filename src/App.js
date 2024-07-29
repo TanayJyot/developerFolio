add7b1553f8b99f31f8e056165e350003f880017
TanayJyot <tjsinghchawla@gmail.com>
2024-07-29 21:37:47 +0530

    Added name in alt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Main from "./containers/Main";
 
 function App() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`;
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+      gtag('config', '${process.env.GTAG}');
+    `;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
   return (
     <div>
       <Main />
-
-      <!-- Google tag (gtag.js) -->
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', '${process.env.GTAG}');
-      </script>
     </div>
   );
 }
 
-// This is to try push
 export default App;
