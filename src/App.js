25c9e52307921dc05e5b03656c57fa7512b7e0e1
TanayJyot <tjsinghchawla@gmail.com>
2024-07-29 21:31:39 +0530

    Added name in alt

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ function App() {
   return (
     <div>
       <Main />
+
+      <!-- Google tag (gtag.js) -->
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.GTAG}');
+      </script>
     </div>
   );
 }
+
 // This is to try push
 export default App;
